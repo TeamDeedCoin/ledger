2f76d9ea56399a3d243e190a5202ffb2ecbec43e
Warren Beyda <warren@deedcoinlaunch.com>
2018-07-09 16:12:18 -0400

    "oops"

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0,
   maximum-scale=1.0">
-  <link href="style.css?v=2" rel="stylesheet">
+  <link href="css/style.css?v=2" rel="stylesheet">
+  <link href="css/animate.css" rel="stylesheet">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 
@@ -18,7 +19,7 @@
   }
   ?>
   </style>
-
+<script type="text/javascript" src="js/wow.js"></script>
 </head>
 <body>
   <div id="pageWrap"> <!-- Page Wrap Begin !-->
