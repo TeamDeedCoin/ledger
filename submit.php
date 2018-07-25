f381ad4407ddaf4d89003fc3919a020633dc25c9
wbdeedcoin <warren@deedcoinlaunch.com>
2018-07-25 13:11:37 -0400

    added captcha v2

--- submit.php
+++ submit.php
@@ -1,5 +1,23 @@
 <?php
-if( !empty($_POST) ) {
+
+$secret = "6LfoXGYUAAAAAGzzhVvhPx-0Yw3scWRlIIE75xtI";
+$response = $_POST["captcha"];
+$remoteip = $_SERVER["REMOTE_ADDR"]; 
+
+$verify=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret={$secret}&response={$response}&remoteip={$remoteoip}");
+
+$captcha_success=json_decode($verify);
+
+/*
+if( $_POST["g-recaptcha-response"] ) {
+    $response = $reCaptcha->verifyResponse(
+        $_SERVER["REMOTE_ADDR"], 
+        $_POST["g-recaptcha-response"]
+    );
+}
+ */
+if( $captcha_success->success == true) {
+
     $name = trim($_POST["name"]);
     $phone = trim($_POST["phone"]);
     $email = $_POST["email"];
@@ -12,11 +30,13 @@ if( !empty($_POST) ) {
     $headers =  'From: <'.$email. ">\r\n" .
         'Reply-To: <'.$email . ">\r\n" .
         'X-Mailer: PHP/' . phpversion();
-    if (@mail('team@ledgerleap.com', $subjects, $content, $headers)){
-        echo "We'll be in contact ...";
-    }else {
-        echo "Mail not sent";
-    }
+
+    @mail('team@ledgerleap.com', $subjects, $content, $headers);
+    echo "<span id='success-message'>We'll be in contact ...</span>";
+
+}else if($captcha_success-> == false){
+    //bad bot go away!
+    echo "<span id='failure-message'>Mail not sent</span>";
 }
 
 function filter($string){
